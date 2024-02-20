5d5368e478d861f0bc875092e640b6f979371ba4
kimseongmin0301 <pose1358@naver.com>
2024-02-20 16:01:40 +0900

    yml 개발 운영 구분 swagger 설정 최신화

--- src/main/java/jpa/study/jpaStudy/config/SwaggerConfig.java
+++ src/main/java/jpa/study/jpaStudy/config/SwaggerConfig.java
@@ -2,18 +2,20 @@ package jpa.study.jpaStudy.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Android Test API")
-                        .description("기본적인 CRUD를 테스트 합니다.")
-                        .version("1.0.0"));
+    public GroupedOpenApi openAPI() {
+        return GroupedOpenApi.builder().group("user")
+                .addOpenApiCustomizer(openApi -> openApi.info(
+                        new Info()
+                                .title("Swagger Test API")
+                                .description("기본적인 CRUD를 테스트 합니다.")
+                                .version("1.0.0"))
+                ).build();
     }
-
 }
